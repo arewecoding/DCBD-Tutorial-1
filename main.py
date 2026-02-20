2214af7730007e525c9f8c09dd1773a817061798
Arew3y <jojilromma@gmail.com>
2026-02-20 16:00:12 +0530

    Added the Python File

---
+++ main.py
@@ -0,0 +1,8 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}, you've just ran the python script of MDS202527 - Rushikesh Vishwasrao")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
